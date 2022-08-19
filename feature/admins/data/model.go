08d9ae63de05f05fe9bce64dcd2075569076947f
ivands26 <ivandwis.fis12@gmail.com>
2022-08-19 21:19:06 +0700

    satuan error

--- feature/admins/data/model.go
+++ feature/admins/data/model.go
@@ -13,7 +13,7 @@ type Product struct {
 	Unit    string `json:"unit" form:"unit" validate:"required"`
 	Stock   int    `json:"stock" form:"stock" validate:"required"`
 	Price   int    `json:"price" form:"price" validate:"required"`
-	Satuan  int    `json:"satuan" form:"satuan" validate:"required"`
+	Satuan  int    `json:"satuan" form:"satuan"`
 	Image   string `json:"product_image" form:"product_image" validate:"required"`
 }
 
